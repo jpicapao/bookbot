573bb9d99a292c550681990fc1248437bd2d605f
Joao <vulliamyjoao@gmail.com>
2024-07-06 16:34:31 +0200

    end of the project

---
+++ main.py
@@ -0,0 +1,53 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(chars_sorted_list, num_words)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(char_list, word_count):
+    print("--- Begin report of books/frankenstein.txt ---\n")
+    print(f"{word_count} words found in the document\n")
+    for item in char_list:
+        if item['char'].isalpha():
+            print(
+                f"The '{item['char']}' character was found {item['num']} times\n")
+    print("--- End report ---")
+
+
+main()
